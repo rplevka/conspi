871684ab27d365b40d1004b20ed10cac914c6fa6
rplevka <rplevka@redhat.com>
2015-08-27 23:56:06 +0200

    conspy.py - better blacklisting and static seed list

--- conspi.py
+++ conspi.py
@@ -8,19 +8,27 @@ afinabul.blog.cz - Juraj Smatana's list of conspiracy-theory webs
 
 import json
 import requests
-import string
 import sys
 from lxml import html
 from urlparse import urlparse, urljoin
 
 blacklist = [
+    'creativecommons.org',
+    'disqus.com',
+    'drupal.org',
     'facebook.com',
     'twitter.com',
+    'gnu.org',
     'google.com',
     'youtube.com',
     'vimeo.com',
+    'weebly.com',
     'wordpress.com',
-    'mojevideo.sk'
+    'messenger.com',
+    'mojevideo.sk',
+    'drupal.org',
+    'linked.in',
+    'linkedin'
 ]
 
 
@@ -45,25 +53,20 @@ def get_seed_list():
             break
         if len(a) > 0:
             web_list.append(a[0].text)
-
     return web_list
 
 
-def get_domain(url):
-    link = '.'.join(string.split(url.netloc, '.')[-2:])
-    link = link.replace('http://', '')
-    return link.replace('www.', '')
+def fix_netloc(netloc):
+    if netloc.startswith('www.'):
+        netloc = netloc.replace('www.', '')
+    return netloc
 
 
-def fix_netloc(url):
-    if url.startswith('www.'):
-        return 'http://' + url
+def is_external(link, web):
+    # print('netlocs: ' + link + ', ' + web.netloc)
+    if link.scheme is '' or link.netloc == web.netloc:
+        return False
     else:
-        return url
-
-
-def is_external(url):
-    if url.scheme is '':
         return True
 
 
@@ -91,50 +94,37 @@ def crawl_web(url, max_breadth=250):
                 for i in page_text.xpath('//a[@href]')
             ]
             for src in page_links:
-                if bool(src.netloc):
-                    web = get_domain(src)
-                    if web not in blacklist:
-                        # wrong! url in this scope might be a local page!!!
-                        if web != get_domain(url):
-                            index = in_array(web, external)
-                            if index is None:
-                                external.append({
-                                    'name': web,
-                                    'score': 1,
-                                    'visited': False,
-                                    'links': [],
-                                })
-                            else:
-                                external[index]['score'] += 1
-                        else:
-                            if src.path and in_array(
-                                urljoin('http://'+web, src.path), internal
-                            ) is None and len(internal) < max_breadth:
-                                internal.append({
-                                    'name': urljoin('http://'+web, src.path),
-                                    'visited': False,
-                                })
-                elif src.path != '/' and src.path and \
-                        len(internal) < max_breadth:
-                    path = urljoin(url.netloc, src.path)
-                    if in_array(path, internal) is None:
-                        internal.append({'name': path, 'visited': False})
-        # except ValueError:
-        #     print(u'ValueError: for url: {}'.format(url))
+                netloc = fix_netloc(src.netloc)
+                if [i for i in blacklist if i in netloc] != []:
+                    print('[blacklist]: skipping ' + netloc)
+                    continue
+                if is_external(src, url):
+                    index = in_array(netloc, external)
+                    if index is None:
+                        external.append({
+                            'name': netloc,
+                            'score': 1,
+                            'visited': False,
+                            'links': [],
+                        })
+                    else:
+                        external[index]['score'] += 1
+                else:
+                    link = urljoin(
+                        src.scheme + '://' + netloc,
+                        src.path
+                    )
+                    if (src.path and in_array(link, internal) is None
+                            and len(internal) < max_breadth):
+                        internal.append({
+                            'name': link,
+                            'visited': False,
+                        })
         except:
-            print "Unexpected error:", sys.exc_info()[0]
-            # raise
-        # except:
-        #     print(
-        #         u'error while fetching the page {0}, skipping. - {1}'
-        #         .format(url, sys.exc_info()[0])
-        #     )
+            print "Unexpected error:", sys.exc_info()[0], sys.exc_info()[1]
 
-    if not url.startswith('http://'):
-        url = 'http://' + fix_netloc(url)
     url = urlparse(url)
-    # print("crawl_page("'http://' + get_domain(url) + ")")
-    crawl_page('http://' + get_domain(url))
+    crawl_page(url.scheme + '://' + url.netloc)
     for link in internal:
         if link['visited'] is False:
             print(
@@ -149,19 +139,59 @@ def crawl_web(url, max_breadth=250):
             )
             link['visited'] = True
             # print("crawl_page(" + link['name'] + ")")
-            crawl_page(urljoin(url.scheme + '://' + url.netloc, link['name']))
+            crawl_page(
+                urljoin(
+                    url.scheme + '://' + fix_netloc(url.netloc),
+                    link['name']
+                )
+            )
     return external
+
+seeds = [
+    'http://www.svetkolemnas.info', 'http://www.zvedavec.org',
+    'http://www.ac24.cz', 'http://www.rodinajezaklad.sk/',
+    'http://www.stopautogenocide.sk', 'http://www.osud.cz',
+    'http://zemejas.cz/', 'http://czech.ruvr.ru/', 'http://slovak.ruvr.ru',
+    'http://www.zemavek.sk/', 'http://panobcan.sk/',
+    'http://www.czechfreepress.cz', 'http://vaseforum.sk/blog/',
+    'http://www.slobodnyvysielac.sk', 'http://www.hlavnespravy.sk',
+    'http://www.badatel.sk', 'http://www.protiprudu.org', 'http://www.beo.sk',
+    'http://obcianskytribunal.sk/blog/', 'http://www.sho.sk',
+    'http://www.voxvictims.com/', 'http://freeglobe.parlamentnilisty.cz/',
+    'http://www.magnificat.sk/', 'http://www.freepub.cz',
+    'http://vkpatriarhat.org.ua/cz/', 'http://www.spolocnostsbm.com/',
+    'http://svobodnenoviny.eu', 'http://www.auria.sk/blog/',
+    'http://afinabul.blog.cz/', 'http://www.dolezite.sk',
+    'http://www.inespravy.sk/', 'http://www.tvina.sk/',
+    'http://www.nadhlad.com/', 'http://www.ze-sveta.cz', 'http://nwoo.org',
+    'http://orgo-net.blogspot.sk', 'http://www.cez-okno.net',
+    'http://www.vlastnihlavou.cz/', 'http://www.neskutocne.sk/',
+    'http://www.bezpolitickekorektnosti.cz', 'http://www.eiaktivity.sk',
+    'http://www.nazorobcana.sk', 'http://www.alternews.cz',
+    'http://pravdive.eu/index.php', 'http://www.aeronet.cz',
+    'http://www.slovenskeslovo.sk/', 'http://www.svobodny-vysilac.cz',
+    'http://www.vedy.sk', 'http://leva-net.webnode.cz',
+    'http://www.novarepublika.cz', 'http://www.extraplus.sk',
+    'http://www.maat.sk', 'http://www.noveslovo.sk',
+    'http://www.lifenews.sk', 'http://www.isstras.eu', 'http://www.borrea.eu',
+    'http://rodobrana.wordpress.com', 'http://www.chelemendik.sk',
+    'http://protiproud.parlamentnilisty.cz',
+    'http://echo24.cz', 'http://www.kontroverznirealita.cz',
+    'http://medzicas.sk'
+]
 
 
 def crawl_from_seed():
-    webs = ['http://'+i for i in get_seed_list() if not i.startswith('http')]
+    # webs = ['http://'+i for i in get_seed_list() if not i.startswith('http')]
+    webs = seeds
     nets = []
     for web in webs:
+        web = urlparse(web)
         nets.append({
-            'name': web,
+            'name': fix_netloc(web.netloc),
             'visited': True,
             'score': 0,
-            'links': crawl_web(web, 500)
+            'links': crawl_web(web.scheme + '://' + fix_netloc(web.netloc), 500)
         })
 
     return json.dumps(nets)
